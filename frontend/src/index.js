ea02772a13e992cba7f5d8dea66f1b222dc10f15
Suveshtr <suvesh.trivedi@gmail.com>
2017-09-25 14:03:59 +0530

    add redux-router-dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import {BrowserRouter} from 'react-router-dom';
+import { createLogger } from 'redux-logger'
+import reducer from './reducers'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const middleware = [ thunk ]
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware),
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+render(
+  <Provider store={store}>
+    <BrowserRouter >
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+)
