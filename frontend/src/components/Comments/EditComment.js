384ad3752be6caa1336f97a3918c7d4b5017c6ac
Suveshtr <suvesh.trivedi@gmail.com>
2017-10-16 18:25:26 +0530

    add edit comment component

---
+++ frontend/src/components/Comments/EditComment.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { withRouter, Link } from 'react-router-dom'
+import CommentForm from './CommentForm'
+import { EditCommentRequest } from '../../actions'
+class EditComment extends React.Component {
+  
+  onSubmit = (comment) => {
+    const { dispatch, history, match } = this.props
+    dispatch(EditCommentRequest(comment))
+    history.push(`/${match.params.category}/posts/${match.params.postId}`)
+  }
+
+  render() {
+    const { comment, match } = this.props
+    return(
+      <div>        
+        <h3>Edit Comment</h3>
+        <Link className="close-create-entity" to={`/${match.params.category}/posts/${match.params.postId}`} >Close</Link>
+        <CommentForm comment={comment} onSubmit={this.onSubmit}/>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state, {match, history}) => {
+  const { comments } = state
+  return {
+    comment: comments[match.params.commentId]
+  }
+}
+export default withRouter(connect(mapStateToProps)(EditComment))
