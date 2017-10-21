f30cf9c5be2109136a1c0b9fba32796421267687
Suveshtr <suvesh.trivedi@gmail.com>
2017-10-21 12:50:59 +0530

    Handle un-defined categories and add validations

--- frontend/src/components/Posts/PostForm.js
+++ frontend/src/components/Posts/PostForm.js
@@ -1,33 +1,32 @@
 import React from 'react'
-import {Form, Col, FormControl, FormGroup, ControlLabel, Button} from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
 class PostForm extends React.Component {
 
   static propTypes = {
-    post: PropTypes.shape( {
-        id: PropTypes.string.isRequired,        
-        timestamp: PropTypes.number.isRequired,
-        title: PropTypes.string.isRequired,
-        body: PropTypes.string.isRequired,
-        author: PropTypes.string.isRequired,
-        category: PropTypes.string.isRequired,       
-      }
-    ).isRequired,  
+    post: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      timestamp: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      body: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+    }
+    ).isRequired,
     onSubmit: PropTypes.func.isRequired
   }
 
   state = {}
 
   handleChange = event => {
-    this.setState( {
+    this.setState({
       [event.target.id]: event.target.value
-    })    
+    })
   }
 
-  handleSubmit = e => {    
+  handleSubmit = e => {
     e.preventDefault()
-    let submitPost = {...this.props.post, ...this.state} 
+    let submitPost = { ...this.props.post, ...this.state }
     this.props.onSubmit(submitPost)
   }
 
@@ -35,72 +34,74 @@ class PostForm extends React.Component {
     const { post } = this.props
     const disabled = (post.title.length && post.author.length && post.body.length)
     return (
-      <div> 
-        
-        
-        <Form horizontal className='create-entity-form' onSubmit={event => this.handleSubmit(event)}>        
-          <FormGroup controlId="title">
-            <Col componentClass={ControlLabel} sm={2}>
-              Title
-            </Col>
-            <Col sm={10}>
-              <FormControl type="text" 
-                  placeholder='Title' 
-                  defaultValue={post.title}
-                  onChange={this.handleChange}
+      <div className='create-entity-form'>
+        <form className="form-horizontal" onSubmit={event => this.handleSubmit(event)}>
+          <div className="create-entity-details" >
+            <lable className="control-label col-sm-2" htmlFor="title">Title</lable>
+
+            <div className=" col-sm-10">
+              <input type="text" id="title"
+                placeholder='Title'
+                required
+                autoFocus
+                defaultValue={post.title}
+                onChange={this.handleChange}
               />
-            </Col>
-          </FormGroup>
-          <FormGroup controlId="author">
-            <Col componentClass={ControlLabel} sm={2}>
+            </div>
+          </div>
+          <div className="create-entity-details">
+            <lable className="control-label col-sm-2" htmlFor="author">
               Author
-            </Col >
-            <Col sm={10}>
-              <FormControl type="text" 
-                  placeholder='Author'
-                  disabled={disabled}
-                  defaultValue={post.author} 
-                  onChange={this.handleChange}
+            </lable >
+            <div className="col-sm-10">
+              <input type="text" id="author"
+                placeholder='Author'
+                required
+                disabled={disabled}
+                defaultValue={post.author}
+                onChange={this.handleChange}
               />
-            </Col>
-          </FormGroup>
-          <FormGroup controlId="body">
-            <Col componentClass={ControlLabel} sm={2}>
+            </div>
+          </div>
+          <div className="create-entity-details">
+            <lable className="control-label col-sm-2" htmlFor="body">
               Description
-            </Col>
-            <Col sm={10}>
-              <FormControl componentClass="textarea" 
-                  placeholder='Description' 
-                  defaultValue={post.body} 
-                  onChange={this.handleChange}
+            </lable>
+            <div className="col-sm-10">
+              <input type="textarea" id="body"
+                placeholder='Description'
+                required
+                defaultValue={post.body}
+                onChange={this.handleChange}
               />
-            </Col>
-          </FormGroup>
-          
-          <FormGroup controlId="category">
-            <Col componentClass={ControlLabel} sm={2}>
+            </div>
+          </div>
+
+          <div className="create-entity-details">
+            <lable className="control-label col-sm-2" htmlFor="category">
               Category
-            </Col>
-            <Col sm={10}>
-              <FormControl componentClass="select" 
-                          onChange={this.handleChange} 
-                          defaultValue={post.category}
-                          disabled={disabled}>                
+            </lable>
+            <div className="col-sm-10">
+
+              <input list="categories" id="category"
+                onChange={this.handleChange}
+                required
+                defaultValue={post.category}                
+                disabled={disabled} />
+              <datalist id="categories">                
                 <option value="react">React</option>
                 <option value="redux">Redux</option>
                 <option value="udacity">Udacity</option>
-              </FormControl>
-            </Col>
-          </FormGroup>
+              </datalist>
+            </div>
+          </div>
 
-          <FormGroup controlId="submitBtn">
-            <Col smOffset={2} sm={10}>
-              <Button type="submit" >
-                Submit
-              </Button>            
-            </Col>
-          </FormGroup>
-        </Form>
+          <div className="create-entity-details">
+            <div className="col-sm-offset-2 col-sm-10">
+              <button type="submit" className="btn btn-default">Submit</button>
+            </div>
+          </div>
+        </form>
       </div>
     )
   }
